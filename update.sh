1c2f8ccf36bf749d1df3cf4b1c2dea3aa979291a
7287vencent <48015638+7287vencent@users.noreply.github.com>
2022-02-21 21:58:37 +0800

    fix(项目): 2022-02-21 21:58:37 合并更新

--- update.sh
+++ update.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # 更新所有
-git submodule foreach git pull 
+# git submodule foreach git pull 
 
 # 提交
+
+
+now_time=$(date "+%Y-%m-%d %H:%M:%S")
+# echo $now_time
+commint="fix(项目): ${now_time} 合并更新"
+echo $commint
+
+git add .
+
+git commit -m "${commint}"
+
+git push
